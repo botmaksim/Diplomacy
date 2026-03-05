2beb3d524898f796fc7b15cd012c18ce9f5089f7
botmaksim <maksimmon2008@gmail.com>
2026-03-05 11:50:00 +0300

    order

--- core/src/main/java/com/diplomacy/orders/Order.java
+++ core/src/main/java/com/diplomacy/orders/Order.java
@@ -1,5 +1,17 @@
 package com.diplomacy.orders;
 
-public class Order {
+public abstract class Order {
 
+    private boolean executable = true;
+
+    public Order() {
+    }
+
+    public boolean isExecutable() {
+        return executable;
+    }
+
+    public void setExecutable(boolean executable) {
+        this.executable = executable;
+    }
 }
