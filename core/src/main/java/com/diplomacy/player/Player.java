011f306a3a7da74bb0fdf7b04070dc631381b76f
botmaksim <maksimmon2008@gmail.com>
2026-03-05 11:40:15 +0300

    player

---
+++ core/src/main/java/com/diplomacy/player/Player.java
@@ -0,0 +1,113 @@
+package com.diplomacy.player;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+import com.diplomacy.units.Unit;
+
+import com.diplomacy.geography.basic.Province;
+
+import com.diplomacy.geography.advanced.Country;
+
+public class Player {
+
+    private final Country country;
+
+    private final String password;
+
+    private List<Unit> units;
+
+    private List<Province> supplyCenters;
+
+    private boolean ordersSubmitted;
+
+    public Player(Country country, String password) {
+
+        this.country = country;
+
+        this.password = password;
+
+        this.units = getInitialUnits(country);
+
+        this.supplyCenters = country.getProvinces();
+
+    }
+
+    public Player(Country country, String password, List<Unit> units, List<Province> supplyCenters) {
+
+        this.country = country;
+
+        this.password = password;
+
+        this.units = units;
+
+        this.supplyCenters = supplyCenters;
+
+    }
+
+    public Country getCountry() {
+
+        return country;
+
+    }
+
+    public List<Unit> getUnits() {
+
+        return units;
+
+    }
+
+    public void setUnits(List<Unit> units) {
+
+        this.units = units;
+
+    }
+
+    public List<Province> getSupplyCenters() {
+
+        return supplyCenters;
+
+    }
+
+    public void setSupplyCenters(List<Province> supplyCenters) {
+
+        this.supplyCenters = supplyCenters;
+
+    }
+
+    public boolean isOrdersSubmitted() {
+
+        return ordersSubmitted;
+
+    }
+
+    public void setOrdersSubmitted(boolean flag) {
+
+        this.ordersSubmitted = flag;
+
+    }
+
+    public boolean tryLogin(String p) {
+
+        return p.equals(password);
+
+    }
+
+    private List<Unit> getInitialUnits(Country c) {
+
+        List<Unit> u = new ArrayList<>();
+
+        for (Province p : c.getProvinces()) {
+
+            if (p.isOccupied()) {
+                u.add(p.getOccupyingUnit());
+            }
+
+        }
+
+        return u;
+
+    }
+
+}
